8b51d017afd551eedddcc7aef466e9989947edea
mohd nur syazwan <syazwan089@gmail.com>
2020-04-06 00:11:48 +0800

    Add project files.

---
+++ OTP.Android/Renderer/MyEntryRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using OTP.CustomRenderer;
+using OTP.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryCustomRenderer),typeof(MyEntryRenderer))]
+namespace OTP.Droid.Renderer
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+                
+            }
+        }
+    }
+}
---
+++ OTP/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OTP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+            box1.BorderColor = Color.ForestGreen;
+        }
+
+        private void Entry_Focused_1(object sender, FocusEventArgs e)
+        {
+            box2.BorderColor = Color.ForestGreen;
+        }
+
+        private void Entry_Focused_2(object sender, FocusEventArgs e)
+        {
+            box3.BorderColor = Color.ForestGreen;
+        }
+
+        private void Entry_Focused_3(object sender, FocusEventArgs e)
+        {
+            box4.BorderColor = Color.ForestGreen;
+        }
+
+        private void box1_Unfocused(object sender, FocusEventArgs e)
+        {
+            box1.BorderColor = Color.Gray;
+            box2.BorderColor = Color.Gray;
+            box3.BorderColor = Color.Gray;
+            box4.BorderColor = Color.Gray;
+        }
+    }
+}
